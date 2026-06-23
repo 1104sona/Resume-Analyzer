d87cbf13d46120cdf422ae00ea77d842d8709fd9
1104sona <sonali.pnd04@gmail.com>
2026-06-24 00:00:52 +0530

    Added Mongodb Atlas Connection

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb://127.0.0.1:27017/resume_analyzer"
+      process.env.MONGO_URL
     );
 
     console.log("MongoDB Connected");
   } catch (error) {
     console.log(error);
+    process.exit(1);
   }
 };
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const resumeRoutes = require("./routes/resumeRoutes");
@@ -12,6 +14,8 @@ app.use(express.json());
 
 app.use("/api", resumeRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
