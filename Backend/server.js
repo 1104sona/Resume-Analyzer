fd7fdde7061dc312963ae562b677e095369943bc
1104sona <sonali.pnd04@gmail.com>
2026-06-24 23:46:50 +0530

    server file updated

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
 const connectDB = require("./config/db");
 
 const app = express();
@@ -11,6 +10,10 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Resume Analyzer Backend Running");
+});
 
 app.use("/api", require("./routes/resumeRoutes"));
 
